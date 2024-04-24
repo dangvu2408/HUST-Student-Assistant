ec4fe75203ab07dbaab5bf477481509a7393d9bb
dangvu2408 <hiend6887@gmail.com>
2024-04-24 15:58:04 +0700

    MPAndroidChart

--- app/src/main/java/com/example/app_01/Service_Fragment.java
+++ app/src/main/java/com/example/app_01/Service_Fragment.java
@@ -1,16 +1,25 @@
 package com.example.app_01;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
+import java.util.ArrayList;
+
 
 public class Service_Fragment extends Fragment {
-    Button btnBarChart, btnLineChart;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,13 +27,34 @@ public class Service_Fragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.service_fragment, container, false);
 
-        btnBarChart = view.findViewById(R.id.bar_chart_btn);
-        btnLineChart = view.findViewById(R.id.line_chart_btn);
+        BarChart barChart = view.findViewById(R.id.bar_chart_display);
 
+        ArrayList<BarEntry> NoOfEmp = new ArrayList<>();
 
+        NoOfEmp.add(new BarEntry(0f, 3.57f));
+        NoOfEmp.add(new BarEntry(1f, 3.63f));
+        NoOfEmp.add(new BarEntry(2f, 3.55f));
 
+        ArrayList<String> semester = new ArrayList<>();
+        semester.add("20221");
+        semester.add("20222");
+        semester.add("20231");
 
-
+        Description description = new Description();
+        description.setText("");
+        barChart.setDescription(description);
+        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(semester));
+        barChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        barChart.getXAxis().setDrawGridLines(false);
+        barChart.getAxisRight().setEnabled(false);
+        barChart.getAxisLeft().setAxisMinimum(2f);
+        barChart.getAxisLeft().setAxisMaximum(4f);
+        BarDataSet barDataSet = new BarDataSet(NoOfEmp, "");
+        barChart.animateY(2000);
+        BarData data = new BarData(barDataSet);
+        data.setBarWidth(0.5f);
+        barDataSet.setColors(Color.rgb(0, 155, 0));
+        barChart.setData(data);
         return view;
     }
 }
