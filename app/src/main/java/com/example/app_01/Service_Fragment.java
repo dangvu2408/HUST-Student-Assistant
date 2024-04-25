07c526238ca5b9bc497ec7aaac5a3dfc99799692
dangvu2408 <hiend6887@gmail.com>
2024-04-25 17:38:19 +0700

    Layout Edition

--- app/src/main/java/com/example/app_01/Service_Fragment.java
+++ app/src/main/java/com/example/app_01/Service_Fragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Description;
@@ -40,6 +41,9 @@ public class Service_Fragment extends Fragment {
         NoOfEmp.add(new BarEntry(2f, 3.55f));
         NoOfEmp.add(new BarEntry(3f, 3.42f));
         NoOfEmp.add(new BarEntry(4f, 3.54f));
+        NoOfEmp.add(new BarEntry(5f, 3.72f));
+        NoOfEmp.add(new BarEntry(6f, 3.80f));
+        NoOfEmp.add(new BarEntry(7f, 4.00f));
         //entry for line chart
         ArrayList<Entry> GPA = new ArrayList<>();
         GPA.add(new Entry(0f, 3.57f));
@@ -47,6 +51,11 @@ public class Service_Fragment extends Fragment {
         GPA.add(new Entry(2f, 3.55f));
         GPA.add(new Entry(3f, 3.42f));
         GPA.add(new Entry(4f, 3.54f));
+        GPA.add(new Entry(5f, 3.72f));
+        GPA.add(new Entry(6f, 3.80f));
+        GPA.add(new Entry(7f, 4.00f));
+
+
 
         ArrayList<String> semester = new ArrayList<>();
         semester.add("20221");
@@ -54,6 +63,9 @@ public class Service_Fragment extends Fragment {
         semester.add("20231");
         semester.add("20232");
         semester.add("20241");
+        semester.add("20242");
+        semester.add("20251");
+        semester.add("20252");
 
         //bar chart data set
         BarDataSet barDataSet = new BarDataSet(NoOfEmp, "");
@@ -67,35 +79,41 @@ public class Service_Fragment extends Fragment {
         barChart.getXAxis().setLabelCount(semester.size());
         barChart.getXAxis().setGranularity(1f);
         barChart.getAxisLeft().setAxisMinimum(2f);
-        barChart.getAxisLeft().setAxisMaximum(4f);
-        barChart.animateY(1000);
+        barChart.getAxisLeft().setAxisMaximum(4.3f);
+        barChart.animateY(500);
         barChart.setFitBars(false);
         BarData data = new BarData(barDataSet);
         data.setBarWidth(0.5f);
         barDataSet.setColors(Color.rgb(0, 155, 0));
+        barDataSet.setLabel("GPA");
         barChart.setData(data);
 
         //line chart data set
-        LineDataSet lineDataSet = new LineDataSet(GPA, "");
-        LineData lineData = new LineData(lineDataSet);
-        lineChart.setDescription(description);
-        lineDataSet.setColor(Color.RED);
-        lineDataSet.setCircleColor(Color.BLUE);
-        lineDataSet.setCircleRadius(4);
-        lineDataSet.setCircleHoleRadius(2);
+        LineDataSet lineDataSetGPA = new LineDataSet(GPA, "");
+
+        LineData lineData = new LineData(lineDataSetGPA);
+
+
+        lineDataSetGPA.setColor(Color.RED);
+        lineDataSetGPA.setCircleColor(Color.BLUE);
+        lineDataSetGPA.setCircleRadius(4);
+        lineDataSetGPA.setCircleHoleRadius(2);
+        lineDataSetGPA.setDrawValues(true);
+        lineDataSetGPA.setLabel("GPA");
+        lineDataSetGPA.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+
         lineChart.setData(lineData);
         lineChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(semester));
         lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
         lineChart.getAxisRight().setEnabled(false);
         lineChart.getAxisLeft().setAxisMinimum(2f);
-        lineChart.getAxisLeft().setAxisMaximum(4f);
-        lineDataSet.setDrawValues(true);
+        lineChart.getAxisLeft().setAxisMaximum(4.3f);
         lineChart.getXAxis().setAxisMinimum(-0.3f);
-        lineChart.getXAxis().setAxisMaximum(4.3f);
+        lineChart.getXAxis().setAxisMaximum((float)(semester.size() - 0.7));
         lineChart.getXAxis().setLabelCount(semester.size());
         lineChart.getXAxis().setGranularity(1f);
-        lineChart.animateX(1000);
-
+        lineChart.animateX(500, Easing.EaseInOutCubic);
+        lineChart.setDescription(description);
         return view;
     }
 }
