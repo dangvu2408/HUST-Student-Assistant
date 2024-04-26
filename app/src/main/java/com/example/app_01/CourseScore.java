3c83f79e1dc7638265daa614a793936ca419beaa
dangvu2408 <hiend6887@gmail.com>
2024-04-26 14:23:43 +0700

    Course Score (New)

---
+++ app/src/main/java/com/example/app_01/CourseScore.java
@@ -0,0 +1,30 @@
+package com.example.app_01;
+
+public class CourseScore {
+    private String CourseID, Alphabet, CourseName;
+    private double QT, CK;
+    private int TC;
+
+    public CourseScore() {}
+    public CourseScore(String CourseName, String CourseID, int TC, double QT, double CK, String Alphabet) {
+        this.CourseName = CourseName;
+        this.CourseID = CourseID;
+        this.TC = TC;
+        this.QT = QT;
+        this.CK = CK;
+        this.Alphabet = Alphabet;
+    }
+
+    public String getCourseName() { return CourseName; }
+    public void setCourseName(String CourseName) { this.CourseName = CourseName; }
+    public String getCourseID() { return CourseID; }
+    public void setCourseID(String CourseID) { this.CourseID = CourseID; }
+    public int getTC() { return TC; }
+    public void setTC(int TC) { this.TC = TC; }
+    public double getQT() { return QT; }
+    public void setQT(double QT) { this.QT = QT; }
+    public double getCK() { return CK; }
+    public void setCK(double CK) { this.CK = CK; }
+    public String getAlphabet() { return Alphabet; }
+    public void setAlphabet(String Alphabet) { this.Alphabet = Alphabet; }
+}
