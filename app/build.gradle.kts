f1ba3dde1a051e67054d6178514b0eaa4bd18761
dangvu2408 <hiend6887@gmail.com>
2024-04-30 17:51:47 +0700

    edited

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ android {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
     implementation("com.google.firebase:firebase-database")
+    //implementation ("com.androidx.support:design:28.0.0")
     //noinspection GradleCompatible
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
--- app/src/main/java/com/example/app_01/Home_Fragment.java
+++ app/src/main/java/com/example/app_01/Home_Fragment.java
@@ -6,58 +6,41 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Home_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Home_Fragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Home_Fragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Home_Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Home_Fragment newInstance(String param1, String param2) {
-        Home_Fragment fragment = new Home_Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    private RecyclerView recyclerView;
+    private ItemsAdapter adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.home_fragment, container, false);
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler);
+        adapter = new ItemsAdapter(getContext());
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 3);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        adapter.setData(getList());
+        recyclerView.setAdapter(adapter);
+        return view;
+    }
+
+    private List<ReItems> getList() {
+        List<ReItems> list = new ArrayList<>();
+        list.add(new ReItems(R.drawable.calendar, "Thời khóa biểu"));
+        list.add(new ReItems(R.drawable.data, "Sắp xếp thời khóa biểu"));
+        list.add(new ReItems(R.drawable.checklist, "Chương trình đào tạo"));
+        list.add(new ReItems(R.drawable.target, "Mục tiêu ra trường"));
+        list.add(new ReItems(R.drawable.money, "Học phí - Công nợ"));
+        list.add(new ReItems(R.drawable.rank, "Bảng xếp hạng"));
+        return list;
     }
 }
