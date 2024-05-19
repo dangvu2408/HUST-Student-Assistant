f8f07eb4cc6c817cae4c2ac2347022403fc6d6a1
dangvu2408 <hiend6887@gmail.com>
2024-05-19 22:52:19 +0700

    cpa/gpa properties

--- app/src/main/java/com/example/app_01/Fragment/Service_Fragment.java
+++ app/src/main/java/com/example/app_01/Fragment/Service_Fragment.java
@@ -1,6 +1,7 @@
 package com.example.app_01.Fragment;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +19,14 @@ import com.example.app_01.Adapter.CustomAdapterScore;
 import com.example.app_01.Constructor.CourseScore;
 import com.example.app_01.R;
 import com.example.app_01.UtilsPack.Utils;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -26,18 +35,19 @@ import java.util.ArrayList;
 
 
 public class Service_Fragment extends Fragment {
+    public LineChart chartCpa;
+    public LineChart chartGpa;
     CustomAdapterScore adapter;
     ArrayList<CourseScore> arrayScore;
     ListView courseList;
-    private String data;
+    private String data, data0;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.service_fragment, container, false);
-        initLayout();
-
         courseList = view.findViewById(R.id.courses);
+        initLayout();
         adapter = new CustomAdapterScore(getContext(), arrayScore);
         courseList.setAdapter(adapter);
         courseList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -47,6 +57,80 @@ public class Service_Fragment extends Fragment {
             }
         });
 
+        chartGpa = view.findViewById(R.id.line_chart_gpa);
+        chartCpa = view.findViewById(R.id.line_chart_cpa);
+        String str = this.data0;
+        if (str != null && !str.equals("")) {
+            try {
+                JSONArray jsonArray = new JSONArray(this.data0);
+                if (jsonArray.length() > 0) {
+                    ArrayList arrayListGPA = new ArrayList();
+                    ArrayList arrayListCPA = new ArrayList();
+                    ArrayList arrayListHK = new ArrayList();
+                    for (int i = jsonArray.length() - 1; i >= 0; i--) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        arrayListGPA.add(new Entry((float)(jsonArray.length() - 1 - i), Float.parseFloat(jsonObject.getString("gpa"))));
+                        arrayListCPA.add(new Entry((float)(jsonArray.length() - 1 - i), Float.parseFloat(jsonObject.getString("cpa"))));
+                        arrayListHK.add(jsonObject.getString("hockihoc"));
+                    }
+                    LineDataSet lineDataSetGPA = new LineDataSet(arrayListGPA, "");
+                    LineData lineDataGPA = new LineData(lineDataSetGPA);
+                    lineDataSetGPA.setValueTextSize(12f);
+                    lineDataSetGPA.setColor(Color.RED);
+                    lineDataSetGPA.setCircleColor(Color.BLUE);
+                    lineDataSetGPA.setCircleRadius(4);
+                    lineDataSetGPA.setCircleHoleRadius(2);
+                    lineDataSetGPA.setDrawValues(true);
+                    lineDataSetGPA.setLabel("GPA");
+                    lineDataSetGPA.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+                    chartGpa.setData(lineDataGPA);
+                    chartGpa.getXAxis().setValueFormatter(new IndexAxisValueFormatter(arrayListHK));
+                    chartGpa.getXAxis().setAxisMaximum((float)(arrayListHK.size() - 0.7));
+                    chartGpa.getXAxis().setLabelCount(arrayListHK.size());
+                    chartGpa.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+                    chartGpa.getAxisRight().setEnabled(false);
+                    chartGpa.getAxisLeft().setAxisMinimum(0f);
+                    chartGpa.getAxisLeft().setAxisMaximum(4.3f);
+                    chartGpa.getXAxis().setAxisMinimum(-0.3f);
+                    chartGpa.getXAxis().setLabelCount(arrayListHK.size());
+                    chartGpa.getXAxis().setGranularity(1f);
+                    chartGpa.animateX(500, Easing.EaseInOutCubic);
+                    Description description = chartGpa.getDescription();
+                    description.setText("");
+                    description.setEnabled(false);
+
+                    LineDataSet lineDataSetCPA = new LineDataSet(arrayListCPA, "");
+                    LineData lineDataCPA = new LineData(lineDataSetCPA);
+                    lineDataSetCPA.setValueTextSize(12f);
+                    lineDataSetCPA.setColor(Color.RED);
+                    lineDataSetCPA.setCircleColor(Color.BLUE);
+                    lineDataSetCPA.setCircleRadius(4);
+                    lineDataSetCPA.setCircleHoleRadius(2);
+                    lineDataSetCPA.setDrawValues(true);
+                    lineDataSetCPA.setLabel("CPA");
+                    lineDataSetCPA.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+                    chartCpa.setData(lineDataCPA);
+                    chartCpa.getXAxis().setValueFormatter(new IndexAxisValueFormatter(arrayListHK));
+                    chartCpa.getXAxis().setAxisMaximum((float)(arrayListHK.size() - 0.7));
+                    chartCpa.getXAxis().setLabelCount(arrayListHK.size());
+                    chartCpa.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+                    chartCpa.getAxisRight().setEnabled(false);
+                    chartCpa.getAxisLeft().setAxisMinimum(0f);
+                    chartCpa.getAxisLeft().setAxisMaximum(4.3f);
+                    chartCpa.getXAxis().setAxisMinimum(-0.3f);
+                    chartCpa.getXAxis().setLabelCount(arrayListHK.size());
+                    chartCpa.getXAxis().setGranularity(1f);
+                    chartCpa.animateX(500, Easing.EaseInOutCubic);
+                    Description description1 = chartCpa.getDescription();
+                    description1.setText("");
+                    description1.setEnabled(false);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
 
         return view;
     }
@@ -54,8 +138,12 @@ public class Service_Fragment extends Fragment {
     private void initLayout() {
         if (getContext() != null) {
             String value = Utils.getInstance().getValueFromSharedPreferences(getContext(),"share_preferences_data", "key_share_preferences_data_diem_thi_ca_nhan");
+            String str = Utils.getInstance().getValueFromSharedPreferences(getContext(),"share_preferences_data", "key_share_preferences_data_diem_gpa_cpa");
             this.data = value;
-            if (value == null || value.equals("") || this.data.equals("[]")) {
+            this.data0 = str;
+            if (value == null || str == null ||
+                    value.equals("") || str.equals("") ||
+                    this.data.equals("[]") || this.data0.equals("[]")) {
                 Toast.makeText(getContext(), "Không tìm thấy thông tin", Toast.LENGTH_SHORT).show();
             } else {
                 showCourseScore();
@@ -117,4 +205,6 @@ public class Service_Fragment extends Fragment {
         txt07.setText(courseScore.getDiemchu());
         dialog.show();
     }
+
+
 }
